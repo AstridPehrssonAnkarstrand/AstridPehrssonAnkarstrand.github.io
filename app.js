915609dfb9cb579330b1eeb15606303b07507a9e
AstridAnkarstrand <astridpehrssonankarstrand@gmail.com>
2025-01-29 21:15:43 +0100

    Vampower content update + wrapper sectioning

--- app.js
+++ app.js
@@ -123,14 +123,20 @@ function showSlides(n) {
 
 // Accordion menu
 var acc = document.getElementsByClassName("accordion");
-//var i;
+var i;
 
-function toggleAccordion(i) {
-    acc[i].classList.toggle("active");
-    var panel = acc[i].nextElementSibling;
-    if (panel.style.maxHeight) {
-        panel.style.maxHeight = null;
-    } else {
-        panel.style.maxHeight = panel.scrollHeight + "px";
-    }
-}
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+
+        /* Toggle between hiding and showing the active panel */
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+} 
